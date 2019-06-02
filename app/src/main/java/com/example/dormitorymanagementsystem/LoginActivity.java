68aa9520d258ce9c8ed7261f88ea9bbe7680a95e
wanghuijuan <1499695895@qq.com>
2019-06-02 20:44:49 +0800

    一大波更新

--- app/src/main/java/com/example/dormitorymanagementsystem/LoginActivity.java
+++ app/src/main/java/com/example/dormitorymanagementsystem/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.dormitorymanagementsystem;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +10,13 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     String TAG="LoginActivity";
     EditText studentID,password;
-    Button login;
+    Button login,back;
     String studentId,passWord;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,19 +25,38 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         studentID=findViewById(R.id.studentID);
         password=findViewById(R.id.password);
         login=findViewById(R.id.login);
+        back=findViewById(R.id.backFromLogin);
 
         login.setOnClickListener(this);
+        back.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {//这个布局里面只有一个按钮设了监听器
-        studentId=studentID.getText().toString();
-        passWord=password.getText().toString();
-        Log.i(TAG, "student: "+studentId+"    "+passWord);
-        //TODO：连接数据库/服务器
-        //现在假设只有studentId=41711042,passWord=41711042时密码正确
-        if(studentId.equals("41711042")&&passWord.equals(("41711042"))){
-            Log.i(TAG, "student: 用户名与密码均正确");
+        if(v.getId()==R.id.login){
+            studentId=studentID.getText().toString();
+            passWord=password.getText().toString();//获得用户输入的两条数据
+            //TODO：连接数据库/服务器
+            //现在假设只有studentId=41711042,passWord=41711042时密码正确
+            if(studentId.equals("41711042")&&passWord.equals(("41711042"))){//用户输入了正确的学号和密码
+                Log.i(TAG, "student: 学号与密码均正确");
+                //6.2加入
+                //把学号和密码放入sharedpreference loginInfo中
+                SharedPreferences loginInfo=getSharedPreferences("loginInfo",0);
+                SharedPreferences.Editor editor = loginInfo.edit();
+                editor.putString("studentId",studentId);
+                editor.putString("passWord",passWord);
+                editor.apply();
+                //跳转回MainActivity
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(LoginActivity.this,"请您输入正确的学号和密码", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if(v.getId()==R.id.backFromLogin){
+            LoginActivity.this.finish();
         }
     }
 }
--- app/src/main/java/com/example/dormitorymanagementsystem/MyFragment.java
+++ app/src/main/java/com/example/dormitorymanagementsystem/MyFragment.java
@@ -1,13 +1,16 @@
 package com.example.dormitorymanagementsystem;
 
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,19 +25,78 @@ public class MyFragment extends Fragment {
     }
 
     //这是我在网上找到的一种实例化fragment布局文件里组件的方法
-    Button login_btn;
+    Button login_btn,personal_info,card_info,notification,suggestion,change_password;
     TextView user_name;
+    String TAG="MyFragment";
+    ImageView avatar;
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
+
+        final SharedPreferences loginInfo=this.getActivity().getSharedPreferences("loginInfo",0);
+        String studentId=loginInfo.getString("studentId","");
+        String passWord=loginInfo.getString("passWord","");
+        Log.i(TAG, "onActivityCreated: "+studentId+"    "+passWord);
+        //如果有的话，studentId和passWord就分别是其值；如果没有的话，就都是空值；截止6.2，尚未报错
+
         login_btn=getActivity().findViewById(R.id.login_btn);
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        personal_info=getActivity().findViewById(R.id.personal_info);
+        card_info=getActivity().findViewById(R.id.card_info);
+        notification=getActivity().findViewById(R.id.notification);
+        suggestion=getActivity().findViewById(R.id.suggestion);
+        change_password=getActivity().findViewById(R.id.change_password);
+        user_name=getActivity().findViewById(R.id.user_name);
+        avatar=getActivity().findViewById(R.id.avatar);
+
+        if (studentId==""&&passWord=="") {//用户还没有登录
+            login_btn.setText("登录");
+            user_name.setText("用户名");
+            avatar.setImageResource(R.drawable.default_avatar);
+            login_btn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
                 /*MainActivity  mainActivity = (MainActivity) getActivity();
                 mainActivity.go2LoginFragment();*/
-                Intent intent = new Intent(getActivity(),LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(intent);
+                }
+            });
+            //点击剩下所有控件都会弹出“请登录”Toast
+            //为了少写几句代码，我写个综合性的View.OnClickListener default_listener放在最下面了
+            personal_info.setOnClickListener(default_listener);
+            card_info.setOnClickListener(default_listener);
+            notification.setOnClickListener(default_listener);
+            suggestion.setOnClickListener(default_listener);
+            change_password.setOnClickListener(default_listener);
+        }
+        else {//用户已经登录
+            login_btn.setText("退出");
+            //退出就是把loginInfo这个sharedpreference清空
+            login_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SharedPreferences.Editor editor = loginInfo.edit();
+                    editor.clear();
+                    editor.apply();
+                    //底下这两句没用。跳转到哪个fragment都不行。必须要重来一遍本fragment创建过程才行
+                    //MainActivity  mainActivity = (MainActivity) getActivity();
+                    //mainActivity.go2DormFragment();
+                    onActivityCreated(null);
+                }
+            });
+            //TODO：正经来说，这里面的图片、用户名应该援引自数据库的
+            //我就先写个示意性的界面
+            user_name.setText("信院老王");
+            avatar.setImageResource(R.drawable.an_avatar);
+            //TODO：一堆跳转过去的activity
+        }
     }
+
+
+    View.OnClickListener default_listener=new View.OnClickListener() {//这个是登录前许多按钮的默认监听器
+        @Override
+        public void onClick(View v) {
+            Toast toast=Toast.makeText(getActivity(),"请您登录账号", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER,0,0);
+            toast.show();
+        }
+    };
 }
